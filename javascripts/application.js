619cfd81fa941db36a0dbd19722ea6e86d2e3192
Timothy Yung <yungsters@gmail.com>
2009-07-31 23:57:22 -0700

    Refactored code for line number effect. Implemented alias replacement effect.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,10 +1,7 @@
-/* Copyright (c) 2009, Timothy Yung. All rights reserved. */
+/* Copyright (c) 2009, Timothy Yung & Anhang Zhu. All rights reserved. */
 
 var YUNG = {
     showLineNumbers: function() {
-        var numListEl = $("<ol id=\"line-numbers\"></ol>").prependTo("body");
-        var maxHeight = numListEl.height();
-        
         var config = {
             duration: 200,   // Fade duration
             delay:    25,    // Delay between adding lines
@@ -12,42 +9,74 @@ var YUNG = {
             normal:   0.1,   // Target opacity
             repeat:   0.5    // Initial opacity for repeats
         };
+        var container = $("<ol id=\"line-numbers\"></ol>").prependTo("body");
+        var numCount = 0;
         
-        var startFade = function(el) {
-            if (el.css("opacity") == config.normal) {
-                el.css({ opacity: config.repeat })
-            }
+        var addNumber = function() {
+            var el = $("<li>" + ++numCount + "</li>").appendTo(container);
             el.fadeTo(config.duration, config.normal);
-        };
-        
-        var addItemEl = function(i) {
-            var el = $("<li>" + i + "</li>").appendTo(numListEl);
-            startFade(el);
             setInterval(function() {
-                startFade(el);
+                el.css({ opacity: config.repeat })
+                el.fadeTo(config.duration, config.normal);
             }, config.interval);
         };
         
-        var itemCount = $("li", numListEl).size();
-        if (itemCount == 0) {
-            addItemEl(1);
-            itemCount += 1;
-        }
-        var oneHeight = $("li", numListEl).height();
+        addNumber();
+        var eachHeight = $("li", container).height();
+        var fullHeight = container.height();
         
-        var addNextEl = function() {
-            if ((itemCount * oneHeight) < maxHeight) {
-                itemCount += 1;
-                addItemEl(itemCount);
-                setTimeout(addNextEl, config.delay);
+        var addUntilFull = function() {
+            if ((numCount * eachHeight) < fullHeight) {
+                addNumber();
+                setTimeout(addUntilFull, config.delay);
             }
         };
-        addNextEl();
+        addUntilFull();
+    },
+    attachAliasEffect: function() {
+        var config = {
+            delay: 70,         // Delay between transforming characters
+            alias: "yungsters" // Target replacement string
+        };
+        var randomChar = function() {
+            return String.fromCharCode(97 + Math.round(Math.random() * 25));
+        };
+        var swapCharAt = function(string, index, char) {
+            return string.substr(0, index) + char + string.substr(index + 1, string.length);
+        };
+        var transformNextChar = function(el) {
+            if (!el.hasOwnProperty("step")) {
+                el.step = 0;
+            }
+            if (el.step < 15) {
+                if (el.step < 5) {
+                    el.innerHTML = swapCharAt(el.innerHTML, el.step + 2, randomChar());
+                } else if (el.step < 6) {
+                    el.innerHTML = swapCharAt(el.innerHTML, el.step + 2, randomChar() + "}");
+                } else if (el.step < 13) {
+                    el.innerHTML = swapCharAt(el.innerHTML, el.step - 4, config.alias.charAt(el.step - 4))
+                } else {
+                    el.innerHTML = swapCharAt(el.innerHTML, 14 - el.step, config.alias.charAt(14 - el.step));
+                }
+                el.step ++;
+                setTimeout(function() {
+                    transformNextChar(el);
+                }, config.delay);
+            }
+        };
+        
+        $("mark").each(function(i) {
+            var el = this;
+            $(el).closest("a").mouseover(function() {
+                transformNextChar(el);
+            });
+        });
     }
 };
 
 $(document).ready(function() {
     YUNG.showLineNumbers();
+    YUNG.attachAliasEffect();
 });
 
 try {
